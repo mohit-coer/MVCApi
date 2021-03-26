4554dba86636b6dcfdd76896f22443838a17ca55
mohit-sofka <mohit.kumar@sofka.com.co>
2021-03-26 21:25:31 +0530

    My Code

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const db =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+mongoose.connect(db, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to Mongoose.....");
+  }
+});
+console.log("called");
+const authRouters = require("./routes/auth");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+app.use("/auth", authRouters);
+
+module.exports = app;
