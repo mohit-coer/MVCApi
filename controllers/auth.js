4fbb51ed4d9e1e8e489be9b70de0ae9e1ac3fb6d
mohit-sofka <mohit.kumar@sofka.com.co>
2021-03-28 11:43:38 +0530

    Latest Code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,40 @@
 const authModel = require("../models/auth");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
   const authData = new authModel(req.body);
-  authData.save((err, savedData) => {
-    if (err) {
-      res.status(400).send({
-        message: err.message,
-      });
-    } else {
-      var token = jwt.sign(savedData, process.env.TOKEN);
-      res.status(201).send({ token: token });
-    }
-  });
+  try {
+    authData.password = await bcrypt.hash(
+      authData.password,
+      await bcrypt.genSalt(10)
+    );
+    authData.save((err, user) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        var token = jwt.sign({ user: user }, process.env.TOKEN);
+        res.status(201).send({ user: user, token: token });
+      }
+    });
+  } catch (ex) {
+    res.status(401).send({ message: ex.message });
+  }
 };
 
 exports.login = async (req, res) => {
   let userData = req.body;
-  authModel.findOne({ email: userData.email }, (error, user) => {
+  authModel.findOne({ email: userData.email }, async (error, user) => {
     if (error) {
       console.log(error);
     } else {
+      const valid_pass = await bcrypt.compare(userData.password, user.password);
       if (!user) {
         res.status(401).send({ message: "Invalid Email" });
-      } else if (user.password !== userData.password) {
+      } else if (valid_pass) {
         res.status(401).send({ message: "Invalid password" });
       } else {
         let token = jwt.sign({ user: user }, process.env.TOKEN);
--- models/auth.js
+++ models/auth.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
-
+const Joi = require("joi");
 const authSchema = mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+function validateUser(user) {
+  const schema = {
+    username: Joi.string().min(5).email(),
+    password: Joi.string().min(5),
+  };
+  return Joi.validate(user, schema);
+}
 
 module.exports = mongoose.model("auth", authSchema);
